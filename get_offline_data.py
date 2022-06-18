77e70972bfe0a93869eed33023adca882bc940d2
Toru Hishinuma <numahha@gmail.com>
2022-06-18 23:43:28 +0900

    Add files via upload

---
+++ get_offline_data.py
@@ -0,0 +1,26 @@
+import gym
+import custom_gym
+import numpy as np
+
+env = gym.make('CustomPendulum-v0')
+
+
+def random_rollout(e):
+    transition_data = []
+    o = env.reset()
+    while True:
+        a = e.action_space.sample()
+        o_next, r, done, _ = env.step(a)
+        transition_data.append(np.hstack([o,a,o_next,r]))
+        o = o_next
+        if done:
+            break
+    return np.array(transition_data)
+
+
+offline_data = []
+for i in range(100):
+    #print(i)
+    offline_data.append(random_rollout(env))
+offline_data = np.array(offline_data)
+np.save('np_offline_data', offline_data)
